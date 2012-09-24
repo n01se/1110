4279caa38b2569bc6e15954044b1b620439fb340
Joel Martin <github@martintribe.org>
2012-09-24 11:51:04 -0700

    server.js: reject large messages. Disconnect test msg.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 "use strict";
 
 var maxClients = 20,
+    maxMsgLength = 500,
     WebSocketServer = require('ws').Server,
     wss = new WebSocketServer( {host: '0.0.0.0', port: 8080}),
     clients = {}, clientData = {}, nextId = 1;
@@ -27,11 +28,17 @@ function sendAll (data) {
     }
 }
 
+function tooManyClients(client, clientId) {
+    console.warn("Too many clients, disconnecting client " +
+                 clientId ? clientId : "");
+    client.close(1008, "Too many clients, try again later");
+}
+
 wss.on('connection', function(client) {
     var numClients = Object.keys(clients).length+1;
 
     if (numClients > maxClients) {
-        client.close(1008, "Too many clients, try again later");
+        tooManyClients(client);
         return;
     }
 
@@ -57,10 +64,21 @@ wss.on('connection', function(client) {
     client.on('message', function(message) {
         var data;
         //console.log("Received message from client ID " + clientId + ": " + message);
+        if (message.length > maxMsgLength) {
+            console.error("client " + clientId + " sent oversize " + message.length + " byte message ");
+            client.close(1009, "Message length too long");
+            return;
+        }
+        else if(message == "testTooManyClients") { // for testing
+            console.log("received testTooManyClients from client " + clientId);
+            tooManyClients(client, clientId);
+            return;
+	} 
         try {
             data = JSON.parse(message);
         } catch (e) {
-            console.error("failed to parse message: " + message);
+            console.error("failed to parse client " + clientId + " message: " + message);
+            return;
         }
         clientData[clientId] = data;
         var obj = {};
